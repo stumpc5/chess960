bed5f3e8f3d698c0a9ce393a4ea08dcb6fc8e2d3
Christian Stump <christian.stump@gmail.com>
2024-09-05 21:41:06 +0200

    changed order of next steps for better readability

--- Code/markdown_templates.py
+++ Code/markdown_templates.py
@@ -6,11 +6,10 @@ header_template = """# Opening Analysis for the board `{board}`
 |:-----------------------:|:---------------:|:--------------:|:---------------:|
 | {nr_matches}            | {percent_white} | {percent_draw} | {percent_black} |
 
+For all openings, the next moves are sorted by likeliness. We only show those with at least 1/3 of the most likely next move.
 """
 
 openings_template = """## Openings for threshold = {threshold}
-
-(The next moves are sorted by likeliness. We only show those with at least 1/3 of the most likely next move.)
 
 | Opening   | Next moves | Likeliness | White wins      | Draw           | Black wins      |
 |-----------|------------|:----------:|:---------------:|:--------------:|:---------------:|
